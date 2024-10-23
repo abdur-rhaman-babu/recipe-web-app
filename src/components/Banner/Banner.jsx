f4a4af1a61bce9c236b5e8cc1760b523fc2a75a3
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-10-23 13:30:24 +0600

    banner design done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,16 @@
+import background from '../../assets/image/banner.jpg'
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="relative mt-16 bg-cover bg-center flex flex-col gap-4 justify-center text-center" style={{backgroundImage:`url(${background})`}}>
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className='relative z-10 w-2/3 mx-auto'>
+            <h1 className='text-6xl font-bold text-center text-white mt-16'>Discover an exceptional cooking class tailored for you!</h1>
+            <p className='text-xl font-semibold text-white my-8'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+            <div className='flex items-center gap-4 justify-center mb-16'>
+                <button className='py-3 px-5 bg-green-400 rounded-badge font-semibold'>Explore Now</button>
+                <button className='py-3 px-5 border border-white text-white rounded-badge font-semibold'>Our Feedback</button>
+            </div>
+            </div>
         </div>
     );
 };
